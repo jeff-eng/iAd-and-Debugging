67ab14fe4ade867ffe965824ed0540109139fb18
Jeff Eng <jeff@jeffeng.me>
2016-08-19 14:49:46 -0700

    created initial project in Xcode

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Jeffrey Eng on 8/19/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
