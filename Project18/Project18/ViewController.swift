b118f9b893c7b25950c2668d341044fe705aad49
Jeff Eng <jeff@jeffeng.me>
2016-08-19 17:39:25 -0700

    created instance of ad banner, set ourself as delegate, and set layout in code to display at bottom

--- Project18/Project18/ViewController.swift
+++ Project18/Project18/ViewController.swift
@@ -6,13 +6,25 @@
 //  Copyright © 2016 Jeffrey Eng. All rights reserved.
 //
 
+import iAd
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ADBannerViewDelegate {
 
+    var bannerView: ADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        bannerView = ADBannerView(adType: .Banner)
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        bannerView.delegate = self
+        bannerView.hidden = true
+        view.addSubview(bannerView)
+        
+        let viewsDictionary = ["bannerView": bannerView]
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
     }
 
     override func didReceiveMemoryWarning() {
